7ae767ad88ce69330ac600d1c07b117908717099
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-22 19:13:17 +0100

    create userForm object

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -2,6 +2,13 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 
+const userForm = {
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    location: '',
+}
 
 export default function SignUpPage () {
     return (
