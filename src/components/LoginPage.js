8854f907dcb49759164c7912b626dee9661e271d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-22 21:49:22 +0100

    create formik for login

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -14,3 +14,29 @@ export default function LoginPage () {
         </div>
     );
 }
+
+function NewUserForm({onSubmitButton}) {
+    return(
+        <Formik
+            render={props => {
+                return (
+                    <Form>
+                        <label>
+                            Name
+                            <Field name='name' type='text' placeholder='Enter your name' />
+                            <ErrorMessage name='name' component='div' />
+                        </label>
+                        <br/>
+                        <label>
+                            username
+                            <Field name='username' type='text' placeholder='Enter your Name' />
+                            <ErrorMessage name='username' component='div' />
+                        </label>
+                        <br/>
+                        <button type='submit'>Login</button>
+                    </Form>
+                )
+            }}
+        />
+    );
+}
