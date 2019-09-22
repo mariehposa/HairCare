ce360428fef6b0a597e7ab8c5cd2f8d147e6dbef
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-22 18:40:53 +0100

    create links for Home, Login and SignUp

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,15 @@ import { Link } from 'react-router-dom';
 export default function Navigation () {
     return (
         <div>
-            
+            <div>
+                <Link to="/">Home</Link>
+            </div>
+            <div>
+                <Link to="/login">Login</Link>
+            </div>
+            <div>
+                <Link to="/signup">SignUp</Link>
+            </div>
         </div>
     );
 }
