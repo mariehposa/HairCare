8f1ee69c2882a3799bcaafb21719863e81f395e2
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-22 18:52:09 +0100

    import formik,error message, yup, etc.

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+
 
 export default function SignUpPage () {
     return (
         <div>
-            
+            <h1>Signup</h1>
         </div>
     );
 }
