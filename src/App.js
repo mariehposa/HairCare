61e2b788653e431fbf50dfec0fe160ada3626538
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-22 17:58:13 +0100

    import homepage to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
-      
+      <HomePage />
     </div>
   );
 }
