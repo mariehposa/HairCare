10c1e5656b054f105bc02305caabecd6add05f2b
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-22 18:08:53 +0100

    import and render signUp page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import HomePage from './components/Homepage';
 import LoginPage from './components/LoginPage';
+import SignUpPage from './components/SignUpPage';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
       <LoginPage />
+      <SignUpPage />
     </div>
   );
 }
