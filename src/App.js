56bd5212ba8a432b95bbc369a0413a458bd44de9
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-22 18:07:45 +0100

    import and render login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/Homepage';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <LoginPage />
     </div>
   );
 }
